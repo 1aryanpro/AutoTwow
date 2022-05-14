ea16b469060767641a8f23e647c1201e620f71c7
Aryan Prodduturi <32280917+SmashMineGame@users.noreply.github.com>
2022-05-13 19:12:10 -0700

    authenticated firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,26 +9,22 @@ export default {
   head: {
     title: 'AutoTwow',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -47,23 +43,27 @@ export default {
       '@nuxtjs/firebase',
       {
         config: {
-          apiKey: '<apiKey>',
-          authDomain: '<authDomain>',
-          projectId: '<projectId>',
-          storageBucket: '<storageBucket>',
-          messagingSenderId: '<messagingSenderId>',
-          appId: '<appId>',
-          measurementId: '<measurementId>'
+          appId: process.env.VUE_APP_FIREBASE_APP_ID,
+          apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+          authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+          storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+          databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+          messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
         },
         services: {
           auth: true,
           firestore: true,
-        }
-      }
-    ]
+        },
+        firestore: {
+          enablePersistence: true,
+          emulatorPort: 8080,
+          emulatorHost: 'localhost',
+        },
+      },
+    ],
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
