846f60774a55ea4f44746fcf67d9b3606e26ecc2
Aryan Prodduturi <32280917+SmashMineGame@users.noreply.github.com>
2022-06-14 11:30:09 -0700

    added page content

--- pages/home.vue
+++ pages/home.vue
@@ -5,7 +5,9 @@ export default {}
 <template>
   <div>
     <h2>What is AutoTWOW?</h2>
-    <h2>Dashboard</h2>
+    <p>AutoTWOW is a website that automates many aspects of hosting and participating in a twow.</p>
+    <p>For participants, our dashboard page shows all the twows your currently participating in and the coresponding responding and voting deadlines. Participants and also respond/vote right on our website without having to look anywhere else for information. The response page also lets users edit their response until the submission deadline and supports multiple responses. On top of voting screens, we also have a voting system called Supervoter which can help you vote on large screens.</p>
+    <p>For hosts, both response and vote collection is automated. Hosts are given a variety of settings they can change to personalize their twow to their needs. Our website can also keep track of your TWOW's leaderboard and eliminations. Each TWOW also has it's own overview page you can use to market your TWOW in other locations. In addtion, we can keep track of all of the standard technicals, such as character count or disallowed characters. It is highly recomended to still create a discord server to interact with participants though.</p>
     <h2>Supervoter</h2>
   </div>
 </template>
